a3a73ffc69955ea4edec20697c45ee3cb4242da0
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-01-20 16:38:56 +0530

    removing include utility from class and moving create_order_at_unified and extract_payment method as class methods

--- app/models/unified_payment/transaction.rb
+++ app/models/unified_payment/transaction.rb
@@ -1,7 +1,24 @@
 module UnifiedPayment
   class Transaction < ActiveRecord::Base
     include Utility
-    self.table_name = "unified_transactions"
-    attr_accessible :gateway_session_id, :gateway_order_id, :url, :merchant_id, :approval_code
+    self.table_name = "unified_payment_transactions"
+    attr_accessible :gateway_session_id, :gateway_order_id, :url, :merchant_id, :approval_code, :xml_response, :pan, :response_description, :response_status, :order_description, :currency, :gateway_order_status
+  
+    def self.create_order_at_unified(amount, options)
+      3.times do |attempt|
+        begin
+          @response = Client.create_order((amount.to_f)*100, options)
+          create(:url => @response['url'], :gateway_order_id => @response['orderId'], :gateway_session_id => @response['sessionId'], xml_response => @response['xml_response'])
+          break
+        rescue
+          @response = false
+        end
+      end
+      @response
+    end
+
+    def self.extract_url_for_unified_payment(response)
+      response['url'] + '?' + 'ORDERID=' + response['orderId'] + '&' + 'SESSIONID=' + response['sessionId']
+    end
   end
 end
--- lib/unified_payment/utility.rb
+++ lib/unified_payment/utility.rb
@@ -1,24 +1,5 @@
 module UnifiedPayment
   module Utility
-    require 'unified_payment/client'
-
-    def create_order_at_unified(amount, options)
-      3.times do |attempt|
-        begin
-          @response = Client.create_order(amount, options)
-          UnifiedPayment::Transaction.create(:url => @response['url'], :gateway_order_id => @response['orderId'], :gateway_session_id => @response['sessionId'])
-          break
-        rescue
-          @response = false
-        end
-      end
-      @response
-    end
-
-    def extract_url_for_unified_payment(response)
-      response['url'] + '?' + 'ORDERID=' + response['orderId'] + '&' + 'SESSIONID=' + response['sessionId']
-    end
-    
     def approved_at_gateway?
       get_unified_order_status == 'APPROVED' 
     end
